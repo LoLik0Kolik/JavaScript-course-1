92f13753668f3bd8c274e85b7ba3600b5db381f6
Nikolai <drejling_kolya_2@mail.ru>
2023-11-06 22:12:01 +0300

    23-24

--- lesson23/script.js
+++ lesson23/script.js
@@ -5,17 +5,7 @@ for (let i = 0; i < 3; i++){
     }
 };
 
-let result = '';
-const length = 7;
-for(let i = 1; i < length; i++){
 
-    for(let j = 0; j < i; j++){
-        result += "#"; 
-    }
-    result += '\n'
-}
-
-console.log(result)
 
 first: for (let i = 0; i < 3; i++){
     console.log(`First level: ${i}`);
@@ -65,3 +55,89 @@ first: for (let i = 0; i < 3; i++){
         }
         num++
     }
+
+
+
+
+
+    // function firstTask() {
+    //     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    //     const arr = [3, 5, 8, 16, 20, 23, 50];
+    //     const result = [];
+    //     for(i=0; i< arr.length ; i++){
+            
+    //         result[i]=arr[i];
+            
+    //     }
+    //     console.log(result);}
+
+        function secondTask() {
+            // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+            const data = [5, 10, 'Shopping', 20, 'Homework'];
+        
+            for(let i= 0; i<data.length; i++)
+            {
+                if (typeof(data[i]) === 'number'){
+                    data[i] = data[i]*2;
+                } else if (typeof(data[i]) === 'string'){
+                    data[i] =`${data[i]} - done`
+                }
+                
+            }
+            console.log(data)
+            
+            // Не трогаем
+            return data;}
+            secondTask();
+            // function secondTask() {
+            //     const data = [5, 10, 'Shopping', 20, 'Homework'];
+            
+            //     for (let i = 0; i < data.length; i++) {
+            //         if (typeof(data[i]) === 'number') {
+            //             data[i] = data[i] * 2;
+            //         } else if (typeof(data[i]) === 'string') {
+            //             data[i] = `${data[i]} - done`;
+            //         }
+            //     }
+            
+            //     console.log(data);
+            //     return data;
+            // }
+            
+            // secondTask();
+
+            // let result = '';
+            // const length = 7;
+            // for(let i = 1; i < length; i++){
+            
+            //     for(let j = 0; j < i; j++){
+            //         result += "#"; 
+            //     }
+            //     result += '\n'
+            // }
+            
+            // console.log(result);
+
+
+            const lines = 5;
+            let result = '';
+            
+            for (let i = 0; i <= lines; i++) {
+                for (let j = 0; j < lines - i; j++) {
+                    result += " ";
+                }
+                for (let j = 0; j < 2 * i + 1; j++) {
+                    result += "*";
+                }
+                result += "\n";
+            }
+            
+            console.log(result)
+
+
+        //      *
+        //     ***
+        //    *****
+        //   *******
+        //  *********
+        // ***********
